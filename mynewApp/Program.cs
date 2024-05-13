41ce8fa9c44d85c439f63ef540b2b53eae4eeabe
jSagar <jugdar.sagar@gmail.com>
2024-05-13 03:51:40 +0000

    create a new code

---
+++ mynewApp/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Write program to add two number
+
+int a = 10;
+int b = 20;
+int c = a + b;
+Console.WriteLine("Sum of a and b is : " + c);
